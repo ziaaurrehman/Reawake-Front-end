c746a7a9aea4890e2313bec006e3d1cfa6c245b1
zia ur rehman <82441680+ziaaurrehman@users.noreply.github.com>
2023-12-25 23:04:58 +0500

    all data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import { Route, Routes } from "react-router-dom";
-// import LandingPage from './Pages/LandingPage';
+import LandingPage from './Pages/LandingPage';
 import Signup from './Pages/Signup';
 import Login from './Pages/Login';
 import Setting from './Pages/Setting';
@@ -11,7 +11,7 @@ function App() {
   return (
 
       <Routes>
-        {/* <Route  path="/" element={<LandingPage />} /> */}
+        <Route  path="/" element={<LandingPage />} />
             <Route  path="/register" element={<Signup />} />
             <Route  path="/login" element={<Login />} />
             <Route path='/setting' element={<Setting/>}/>
