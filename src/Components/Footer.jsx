f6ac38984981225bd9a5bea9aa0eb1587ea5909a
zia ur rehman <82441680+ziaaurrehman@users.noreply.github.com>
2023-12-27 22:09:14 +0500

    social -links

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 const Footer = () => {
   return (
     <div className='footer'>
@@ -24,22 +24,30 @@ const Footer = () => {
             <div className=''>
                 <img src='/images/Group 388.svg' alt=''/>
             </div>
+            <Link to="https://www.facebook.com">
             <div className='d-flex pt-1'>
                 <img className='icon2' src='/images/fb.svg' alt=''/>
                 <div className='footer_p pt-2 pl-2'>Facebook</div>
             </div>
+            </Link>
+            <Link to="https://www.linkedin.com/">
             <div className='d-flex pt-1'>
                 <img className='icon2' src='/images/in.svg' alt=''/>
                 <div className='footer_p pt-1 pl-2'>Linkedin</div>
             </div>
+            </Link>
+            <Link to="https://www.instagram.com/">
             <div className='d-flex pt-1'>
                 <img className='icon2' src='/images/insta.svg' alt=''/>
                 <div className='footer_p pt-1 pl-2'>Instagram</div>
             </div>
+            </Link>
+            <Link to="https://www.youtube.com/">
             <div className='d-flex pt-1'>
                 <img className='icon2' src='/images/you.svg' alt=''/>
                 <div className='footer_p pt-1 pl-2'>Youtube</div>
             </div>
+            </Link>
         </div>
     </div>
   )
